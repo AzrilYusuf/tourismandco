feb82b4c009389a6de3df70cd877f75ab101ec29
AzrilYusuf <muhammadazrilyusuf.w3rk@gmail.com>
2025-10-03 19:19:58 +0700

    feat(style): Install googlefonts library; Create style.dart to store const for style the app; Implement font from googlefonts.

--- lib/app.dart
+++ lib/app.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'screens/location_detail/location_detail.dart';
+import 'style.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -9,6 +10,23 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: LocationDetail(),
+      theme: ThemeData( // This is how to set the theme of the application
+        appBarTheme: AppBarTheme( // Set the theme of the app bar
+          titleTextStyle: fontNameDefault.copyWith(fontSize: mediumTextSize, fontWeight: FontWeight.w300),
+        ),
+      ),
     );
   }
 }
+
+// This is how to set the theme of the application (alt way)
+// theme: _buildTheme(Brightness.light), // brightness is set to light (this is when _buildTheme is called)
+
+// This is the theme of your application. (alt way)
+// ThemeData _buildTheme(Brightness brightness) {
+//   final ThemeData baseTheme = ThemeData(brightness: brightness);
+
+//   return baseTheme.copyWith(
+//     textTheme: GoogleFonts.montserratTextTheme(baseTheme.textTheme), // Text theme used by the app
+//   );
+// }
---
+++ lib/style.dart
@@ -0,0 +1,10 @@
+// This is the file where you can add your own styles to the app.
+
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+const largeTextSize = 26.0;
+const mediumTextSize = 20.0;
+const bodyTextSize = 16.0;
+
+TextStyle fontNameDefault = GoogleFonts.montserrat();
